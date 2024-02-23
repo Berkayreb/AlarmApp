fd8cfebd490beba68485af857a09e590a57a0404
Berkay <berkayreb@gmail.com>
2024-02-23 16:24:11 +0300

    .

---
+++ AlarmApp.Domain/Interfaces/ITodoItemRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlarmApp.Domain.Entities;
+
+namespace AlarmApp.Domain.Repository
+{
+    public interface ITodoItemRepository
+    {
+        Task<List<TodoItem>> GetListAsync();
+
+        Task<TodoItem> GetAsync(Guid id);
+
+        Task<TodoItem> InsertAsync(TodoItem todoItem);
+
+        Task<TodoItem> UpdateAsync(Guid id, TodoItem todoItem);
+
+        Task DeleteAsync(Guid id);
+    }
+}
---
+++ AlarmApp.Infrastructure/Data/Repositories/TodoItemRepository.cs
@@ -0,0 +1,13 @@
+﻿using AlarmApp.Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlarmApp.Infrastructure.Data.Repositories
+{
+    public class TodoItemRepository
+    {
+    }
+}
